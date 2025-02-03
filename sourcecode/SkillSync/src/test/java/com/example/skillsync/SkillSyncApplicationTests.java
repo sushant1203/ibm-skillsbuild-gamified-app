1509921383d6900fcb5866d249ee5c44c8206616
Ali Ebtehaj <mhalia.ebtehaj@gmail.com>
2025-02-03 18:49:27 +0000

    Create springBoot

---
+++ sourcecode/SkillSync/src/test/java/com/example/skillsync/SkillSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
