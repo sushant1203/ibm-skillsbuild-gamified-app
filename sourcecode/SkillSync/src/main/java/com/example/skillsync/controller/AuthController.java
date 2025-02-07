cd2dac1b060171a3ad31b1a65fbed7aa0f31f5bb
Sushant <sjk50@student.le.ac.uk>
2025-02-07 00:34:21 +0000

    defined controller class with registration page nad log in page. Implemented repository methods(findBy) for registration. Success message if successful.

---
+++ sourcecode/SkillSync/src/main/java/com/example/skillsync/controller/AuthController.java
@@ -0,0 +1,67 @@
+package com.example.skillsync.controller;
+
+import com.example.skillsync.model.User;
+import com.example.skillsync.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    //shows the registration page
+    @GetMapping("/register")
+    public String showRegistrationPage() {
+        return "register";
+    }
+
+    //handles user registration
+    @PostMapping("/register")
+    public String registerUser(@RequestParam String name,
+                               @RequestParam String email,
+                               @RequestParam String username,
+                               @RequestParam String password,
+                               Model model) {
+
+        //check if username or email already exists
+        if (userService.findByUsername(username) != null) {
+            model.addAttribute("error","username already exists!choose another one.");
+            return "register";//reloads registration page with error message, applies same with email
+        }
+        if (userService.findByEmail(email) != null) {
+            model.addAttribute("error","email is already registered!use another email.");
+            return "register";
+        }
+
+        //create a user object and save it
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setScore(0);//set a default score at registration(0)
+
+        userService.registerUser(user);
+
+        //redirects user to login page with success message
+        return "redirect:/login?success=true";
+    }
+
+    //displays the login page
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    //shows the user dashboard after login
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+        return "dashboard";
+    }
+}
