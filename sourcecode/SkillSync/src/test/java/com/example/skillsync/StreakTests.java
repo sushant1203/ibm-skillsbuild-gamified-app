531b0adbb7f3c3c923d162556429b0d5195ba149
jasperchr <148618386+jasperchr@users.noreply.github.com>
2025-03-21 15:53:05 +0000

    Added annotation to tests + experimental mockmvc testing (not for actual usage)

--- sourcecode/SkillSync/src/test/java/com/example/skillsync/StreakTests.java
+++ sourcecode/SkillSync/src/test/java/com/example/skillsync/StreakTests.java
@@ -1,5 +1,6 @@
 package com.example.skillsync;
 
+import com.example.skillsync.controller.AuthController;
 import com.example.skillsync.model.User;
 import com.example.skillsync.repo.UserRepository;
 import com.example.skillsync.service.UserService;
@@ -8,12 +9,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
@@ -25,32 +31,51 @@ public class StreakTests {
     @MockitoBean
     private UserRepository userRepository;
 
+    @Autowired
+    private AuthController authController;
+
     private User user;
+
+    private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        this.user = new User();
+        user = new User(); // Setting up user for test
         user.setName("TestUser");
         user.setStreak(3);
 
+        mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
     }
 
-    @Test
+    @Test // Testing the streak increase by setting the user's last login to the day before, and updating streak
     void testStreakIncrease() {
-        user.setLastLogin(LocalDate.now().minusDays(1));
+        user.setLastLogin(LocalDate.now().minusDays(1)); // Sets to yesterday
         when(userRepository.findByUsername("TestUser")).thenReturn(user);
-        userService.setStreak(user);
-        assertEquals(4, user.getStreak());
+        userService.setStreak(user); // Setting streak
+        assertEquals(4, user.getStreak()); // Checking result
         verify(userRepository).save(user);
     }
 
-    @Test
+    @Test // Testing the streak by setting the user's last login to 2 days ago, so update should reset streak
     void testStreakReset() {
-        user.setLastLogin(LocalDate.now().minusDays(2));
+        user.setLastLogin(LocalDate.now().minusDays(2)); // Sets to 2 days ago
         when(userRepository.findByUsername("TestUser")).thenReturn(user);
-        userService.setStreak(user);
-        assertEquals(1, user.getStreak());
+        userService.setStreak(user); // Setting streak
+        assertEquals(1, user.getStreak()); // Checking result
         verify(userRepository).save(user);
+    }
+
+    @Test // Not in use
+    void testStreakRegister() throws Exception {
+        mockMvc.perform(post("/register")
+                .param("name","TestUser1")
+                .param("password","TestUser1!")
+                .param("username","TestUser1")
+                .param("email","TestUser@Email.com")
+        );
+        when(userRepository.findByUsername("TestUser1")).thenReturn(user);
+        userRepository.save(user);
+        assertEquals(1, user.getStreak());
     }
 
 }
