adf1b600057610817805f3d621f16983579b27e2
a17abm <abmaa999@gmail.com>
2025-03-25 17:03:31 +0000

    Add user notification queries

--- sourcecode/SkillSync/src/main/java/com/example/skillsync/repo/UserRepository.java
+++ sourcecode/SkillSync/src/main/java/com/example/skillsync/repo/UserRepository.java
@@ -1,16 +1,22 @@
 package com.example.skillsync.repo;
+
 import com.example.skillsync.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
 
-
-
-// This interface extends the JpaRepository interface which provides CRUD operations for the User entity.
+// This interface extends the JpaRepository interface, which provides CRUD operations for the User entity.
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // find user by username
+    // Find user by username
     User findByUsername(String username);
 
-    // find user by email
+    // Find user by email
     User findByEmail(String email);
-}
 
+    // Custom query to find users who haven't been notified in the last 3 days
+    List<User> findByLastNotifiedDateBefore(LocalDate date);
+
+    // New method: Find users with incomplete courses
+    List<User> findByHasIncompleteCoursesTrue();
+}
